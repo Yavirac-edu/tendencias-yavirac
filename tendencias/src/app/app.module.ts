7fbd1be9b252d5efc3a5a0d90c01f7444945ab05
Steveen Ordoñez <zh311505@gmail.com>
2022-11-21 23:00:12 -0500

    feat:implementar componentes en shared

--- tendencias/src/app/app.module.ts
+++ tendencias/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    NopagesfoundComponent
+    NopagesfoundComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
