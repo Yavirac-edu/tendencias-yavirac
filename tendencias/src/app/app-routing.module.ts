ff94778c60b1e308ad1d96b175bf4b069a6b8a62
Steveen Ordoñez <zh311505@gmail.com>
2022-11-25 01:43:16 -0500

    feat:implementar, login,register,NotPagesFound y css

--- tendencias/src/app/app-routing.module.ts
+++ tendencias/src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
-  {path: '', component:AppComponent ,
-  children: [
-    {path:'login-tenden',component:LoginComponent},
-    {path:'rehister',component:RegisterComponent}
-  ]}
+  {path: "login-form", component: LoginComponent },
+  {path: "register-form", component: RegisterComponent },
 ];
 
 @NgModule({
--- tendencias/src/app/app.module.ts
+++ tendencias/src/app/app.module.ts
@@ -1,32 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    NopagesfoundComponent,
     SidebarComponent,
     HeaderComponent,
     FooterComponent,
     BreadcrumbsComponent,
-    LoginComponent,
-    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
