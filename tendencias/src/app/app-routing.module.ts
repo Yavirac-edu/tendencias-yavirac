2feb7fdd0beb64804beba8be4d6384a1b7a0ae61
Steveen Ordoñez <zh311505@gmail.com>
2022-11-27 18:46:39 -0500

    feat:implementar routing hijas en shred, pages y cambiar nopagesFound

--- tendencias/src/app/app-routing.module.ts
+++ tendencias/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { CatalogueComponent } from './pages/catalogue/catalogue.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
+import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
 import { PagesComponent } from './pages/pages.component';
 import { ProductComponent } from './pages/product/product.component';
 
