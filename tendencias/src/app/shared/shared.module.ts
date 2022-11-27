0c155d91c026d76a46bfc899adf1df80ab5ba08c
Steveen Ordoñez <zh311505@gmail.com>
2022-11-27 18:41:23 -0500

    fix:solucionar html

--- tendencias/src/app/shared/shared.module.ts
+++ tendencias/src/app/shared/shared.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { AppRoutingModule } from '../app-routing.module';
+
 
 
 
